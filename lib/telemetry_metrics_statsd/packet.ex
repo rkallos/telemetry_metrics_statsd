17b4ac201a980caea32f8caff2bd6e6050c4a15d
Cola Cheng <cola.cheng@grindr.com>
2023-04-11 19:30:19 +0800

    Make packet module support buffering.

--- lib/telemetry_metrics_statsd/packet.ex
+++ lib/telemetry_metrics_statsd/packet.ex
@@ -1,6 +1,69 @@
 defmodule TelemetryMetricsStatsd.Packet do
   @moduledoc false
 
+  require Logger
+  alias __MODULE__
+
+  @type t :: %__MODULE__{
+          max_bytes: integer(),
+          max_age_micro: integer(),
+          send_fun: function(),
+          data: iodata(),
+          bytes: integer(),
+          birth_micro: integer()
+        }
+
+  defstruct max_bytes: nil,
+            max_age_micro: nil,
+            send_fun: nil,
+            data: [],
+            bytes: 0,
+            birth_micro: nil
+
+  @spec new(max_bytes :: integer(), max_age_micro :: integer(), send_fun :: function()) :: Packet.t()
+  def new(max_bytes, max_age_micro, send_fun) do
+    do_new(max_bytes, max_age_micro, send_fun)
+  end
+
+  @spec get_timeout(Packet.t()) :: integer()
+  def get_timeout(%Packet{max_age_micro: max_age_micro, birth_micro: birth_micro}) do
+    case max_age_micro - (now() - birth_micro) do
+      t when t < 1000 -> 0
+      t -> div(t, 1000)
+    end
+  end
+
+  @spec maybe_send(Packet.t(), iodata()) :: Packet.t()
+  def maybe_send(packet, line) when is_list(line) do
+    maybe_send(packet, IO.iodata_to_binary(line))
+  end
+
+  def maybe_send(packet, "") do
+    maybe_send(packet)
+  end
+
+  def maybe_send(%Packet{max_bytes: max_bytes} = packet, line) when byte_size(line) > max_bytes do
+    Logger.error("discarded due to packet size overflow:\nmetric=#{inspect(line)}")
+    maybe_send(packet)
+  end
+
+  def maybe_send(%Packet{bytes: bytes, data: data, max_bytes: max_bytes} = packet, line) when is_binary(line) do
+    new_bytes = concatenated_size(bytes, byte_size(line))
+    case new_bytes > max_bytes do
+      true ->
+        new_packet = flush_send(packet)
+        maybe_send(new_packet, line)
+      false ->
+        maybe_send(%Packet{packet | data: concatenate(data, line), bytes: new_bytes}, line)
+    end
+  end
+
+  @spec flush_send(Packet.t()) :: Packet.t()
+  def flush_send(%Packet{data: data, bytes: bytes, max_bytes: max_bytes, max_age_micro: max_age_micro, send_fun: send_fun}) do
+    bytes > 0 && send_fun.(data)
+    do_new(max_bytes, max_age_micro, send_fun)
+  end
+
   @spec build_packets([binary()], size :: non_neg_integer(), joiner :: binary()) :: [binary()]
   def build_packets(binaries, max_size, joiner)
       when is_integer(max_size) and max_size > 0 and is_binary(joiner) do
@@ -50,5 +113,25 @@ defmodule TelemetryMetricsStatsd.Packet do
         packet | acc
       ])
     end
+  end
+
+  defp maybe_send(packet) do
+    case get_timeout(packet) == 0 do
+      true  -> flush_send(packet)
+      false -> packet
+    end
+  end
+
+
+  defp concatenated_size(0, new_line_bytes), do: new_line_bytes
+  defp concatenated_size(bytes, new_line_bytes), do: bytes + new_line_bytes + 1
+
+  defp concatenate([], line), do: [line]
+  defp concatenate(acc_lines, line), do: [acc_lines, ?\n, line]
+
+  defp now(), do: System.system_time(:microsecond)
+
+  defp do_new(max_bytes, max_age_micro, send_fun) do
+    %Packet{max_bytes: max_bytes, max_age_micro: max_age_micro, send_fun: send_fun, birth_micro: now()}
   end
 end
